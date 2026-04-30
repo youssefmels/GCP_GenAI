78a022096cbf6819a6660d3e3bd65ca3ca42e838
Abdallah1321 <abdallah.hussein.ibrahim@gmail.com>
2026-04-30 10:23:08 +0300

    test

--- gcp_run.py
+++
@@ -1,9 +0,0 @@
-import google.auth
-from google.adk.tools.bigquery import BigQueryToolset, BigQueryCredentialsConfig
-
-# Load Application Default Credentials
-credentials, project_id = google.auth.default()
-
-# Configure the toolset
-credentials_config = BigQueryCredentialsConfig(credentials=credentials)
-bigquery_toolset = BigQueryToolset(credentials_config=credentials_config)
